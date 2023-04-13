c09d07b335d1e7598eb418d65003fabd4bfcfd1a
Valentin Lafranca <valentin.lafranca@gmail.com>
2023-04-13 19:31:41 -0400

    Basic page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,53 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
 
-const API_URL = 'http://localhost:8099'
+const API_URL = "http://localhost:8099";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Planned Test</h1>
-      <div>
-        <button type="button">Retrieve Users</button>
+    <>
+      <div className="sticky-bar">
+        <img src="/logo.svg" />
+        <h1>Planned Test</h1>
       </div>
-      <div>
-        <h2>Users</h2>
-        min: <input name="minAge" value="0" type="number" />
-        max: <input name="maxAge" value="100" type="number" />
-        <button type="button">Filter by age</button>
+      <div className="App">
+        <div className="container">
+          <h2>Users</h2>
+          <div className="col-layout">
+            <div className="col-2 box">
+              <div className="filter-container">
+                <input name="minAge" value="0" type="number" />
+                <input name="maxAge" value="100" type="number" />
+                <button type="button" style={{ alignSelf: "flex-start" }}>
+                  Filter by age
+                </button>
+              </div>
+            </div>
+            <div className="col-3 box">
+              <div className="search-bar">
+                <input type="text" placeholder="Search user" />
+              </div>
+              <div className="table-row table-header">
+                <div></div>
+                <div>
+                  Name
+                  <img src="sort-arrows.svg" />
+                </div>
+                <div>
+                  Age <img src="sort-arrows.svg" />
+                </div>
+              </div>
+              <div className="table-row">
+                <div>
+                  <input type="checkbox" />
+                </div>
+                <div>Name</div>
+                <div>Truc</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
